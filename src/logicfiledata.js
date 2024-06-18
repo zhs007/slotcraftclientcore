a2dc2e8fb5594881e5565de8d6df02caf62c514b
zhs007 <sssxueren@gmail.com>
2024-06-18 17:29:19 +0800

    Supported new json data

--- src/logicfiledata.js
+++ src/logicfiledata.js
@@ -15,6 +15,41 @@ function parseValWeight(data, isAdmin) {
         }
 
         return arr;
+    } else if (data.excelJson) {
+        let lst = [];
+        let arr = [];
+        let header = ['val', 'weight'];
+        arr.push(header);
+
+        for (let i = 0; i < data.excelJson.length; i++) {
+            const curdata = data.excelJson[i];
+            if (isAdmin) {
+                arr.push([curdata.val.trim(), curdata.weight.trim()]);
+            } else {
+                arr.push([curdata.val.trim(), '?']);
+            }
+        }
+
+        return arr;
+    }
+
+    return undefined;
+}
+
+function parseValMapping(data) {
+    if (data.excelJson) {
+        let lst = [];
+        let arr = [];
+        let header = ['in', 'out'];
+        arr.push(header);
+
+        for (let i = 0; i < data.excelJson.length; i++) {
+            const curdata = data.excelJson[i];
+
+            arr.push([curdata.in.trim(), curdata.out.trim()]);
+        }
+
+        return arr;
     }
 
     return undefined;
@@ -91,6 +126,52 @@ function parseReels(data, paytable, isAdmin) {
     throw new Error('Empty reels data. ' + data.fileName);
 }
 
+function parseLinedata(data) {
+    if (data.excelJson && data.excelJson.length > 0) {
+        let lst = [];
+        let arr = [];
+
+        let header = ['line'];
+        let maxReel = getMaxInReels(data.excelJson[0]);
+
+        for (let i = 0; i < maxReel; i++) {
+            header.push('r' + (i + 1));
+        }
+
+        arr.push(header);
+
+        for (let i = 0; i < data.excelJson.length; i++) {
+            let curarr = [(i + 1).toString()];
+
+            for (let j = 0; j < maxReel; j++) {
+                curarr.push(data.excelJson[i]['R' + (j + 1)])
+            }
+
+            // for (let j = 0; j < lst.length; j++) {
+            //     if (isAdmin) {
+            //         if (lst[j][paytable[i][0]]) {
+            //             curarr.push(lst[j][paytable[i][0]]);
+            //         } else {
+            //             curarr.push('×');
+            //         }
+            //     } else {
+            //         if (lst[j][paytable[i][0]]) {
+            //             curarr.push('√');
+            //         } else {
+            //             curarr.push('×');
+            //         }
+            //     }
+            // }
+
+            arr.push(curarr);
+        }
+
+        return arr;
+    }
+
+    throw new Error('Empty reels data. ' + data.fileName);
+}
+
 function getMaxInPaytableSymbol(symbolData) {
     for (i = 1; i < 999; i++) {
         if (!symbolData.hasOwnProperty('X' + i)) {
@@ -151,31 +232,61 @@ function parsePaytable(paytableData) {
     throw new Error('Empty paytable data.');
 }
 
+function parsePaytable2(paytableData) {
+    let maxNum = getMaxInPaytable(paytableData);
+    let arr = [];
+
+    for (let i = 0; i < paytableData.length; i++) {
+        const symbolData = paytableData[i];
+
+        if (i == 0) {
+            let header = ['symbol'];
+
+            for (let j = 0; j < maxNum; j++) {
+                header.push('x' + (j + 1));
+            }
+
+            arr.push(header);
+        }
+
+        let curarr = [symbolData.Symbol];
+
+        for (let j = 0; j < maxNum; j++) {
+            const curv = parseInt(symbolData['X' + (j + 1)]);
+            if (curv == NaN) {
+                throw new Error('Invalid paytable data. ' + symbolData.Symbol + '.' + ('X' + (j + 1)));
+            }
+
+            curarr.push(curv);
+        }
+
+        arr.push(curarr);
+    }
+
+    return arr;
+}
+
 function genLogicFileData(repository, isAdmin) {
     let logicFiles = {};
 
-    if (repository.paytableList) {
-        if (repository.paytableList.length == 0) {
+    if (repository.paytableData) {
+        if (repository.paytableData.length == 0) {
             throw new Error('No paytable data.');
         }
 
-        if (repository.paytableList.length > 1) {
-            throw new Error('There are multiple paytable data.');
+        const arr = parsePaytable2(repository.paytableData);
+        if (!arr) {
+            throw new Error('Invalid paytable data.');
         }
 
-        for (let i = 0; i < repository.paytableList.length; i++) {
-            const arr = parsePaytable(repository.paytableList[i]);
-            if (!arr) {
-                throw new Error('Invalid paytable data.');
-            }
-
-            logicFiles['paytable'] = {
-                name: repository.paytableList[i].name,
-                fileName: repository.paytableList[i].fileName,
-                type: repository.paytableList[i].type,
-                data: arr,
-            };
-        }
+        logicFiles['paytable'] = {
+            name: repository.paytableList[0].name,
+            fileName: repository.paytableList[0].fileName,
+            type: repository.paytableList[0].type,
+            data: arr,
+        };
+    } else {
+        throw new Error('No paytable data.');
     }
 
     if (repository.otherList) {
@@ -197,8 +308,33 @@ function genLogicFileData(repository, isAdmin) {
                     type: data.type,
                     data: arr,
                 };
-            } else if (data.type == 'StringValWeight' || data.type == 'ReelSetWeight') {
+            } else if (data.type == 'Linedata') {
+                const arr = parseLinedata(data);
+                if (!arr) {
+                    throw new Error('Invalid Line data. - ' + data.fileName);
+                }
+
+                logicFiles[data.fileName] = {
+                    name: data.name,
+                    fileName: data.fileName,
+                    type: data.type,
+                    data: arr,
+                };
+            } else if (data.type == 'StringValWeight' || data.type == 'ReelSetWeight' || data.type == 'SymbolWeight' || data.type == 'IntValWeight') {
                 const arr = parseValWeight(data, isAdmin);
+                if (!arr) {
+                    throw new Error('Invalid ' + data.type + ' data. - ' + data.fileName);
+                }
+
+                // console.log(arr);
+                logicFiles[data.fileName] = {
+                    name: data.name,
+                    fileName: data.fileName,
+                    type: data.type,
+                    data: arr,
+                };
+            } else if (data.type == 'IntValMappingFile' || data.type == 'StringValMappingFile') {
+                const arr = parseValMapping(data);
                 if (!arr) {
                     throw new Error('Invalid ' + data.type + ' data. - ' + data.fileName);
                 }
