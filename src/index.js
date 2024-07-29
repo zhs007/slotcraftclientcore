95daded87cd52844af7f4cbbabea4d5977cb8ce6
zhs007 <sssxueren@gmail.com>
2024-07-29 15:47:32 +0800

    fixed a bug for exports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { SCLogicMgr } = require('./logicmgr.js');
+const { SCLogicMgr2 } = require('./logicmgr2.js');
 const { parseSlotCraftJson } = require('./utils.js');
 const { genLogicData } = require('./logicdatautils.js');
 const { genLogicFileData } = require('./logicfiledata.js');
@@ -10,9 +11,11 @@ let SlotcraftEditor = {
 };
 
 globalThis.SCLogicMgr = SCLogicMgr;
+globalThis.SCLogicMgr2 = SCLogicMgr2;
 globalThis.parseSlotCraftJson = parseSlotCraftJson;
 globalThis.SlotcraftEditor = SlotcraftEditor;
 
 exports.SCLogicMgr = SCLogicMgr;
+exports.SCLogicMgr2 = SCLogicMgr2;
 exports.parseSlotCraftJson = parseSlotCraftJson;
 exports.SlotcraftEditor = SlotcraftEditor;
